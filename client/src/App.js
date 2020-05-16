53fc4a89cd3e5f36d5bb522a6f4c8b93559f06ad
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-05-15 17:06:43 -0700

    update protected routes directory name

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import { AuthProvider, useAuth } from './utils/auth';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
-import Home from './pages/Protected Route';
+import Home from './pages/ProtectedRoute';
 import Navbar from './components/Navbar';
 
 function ProtectedRoute({ children, ...rest }) {
