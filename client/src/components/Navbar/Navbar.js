56314210f51e7f2592218fae3fa671014540ec90
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-05-16 17:53:52 -0700

    fix navbar import

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Navbar.scss';
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
-import { useAuth } from '../../utils/auth';
+// import { useAuth } from '../../utils/auth';
 import Logo from '../../components/Logo/Logo';
 import LogoText from '../../components/LogoText/LogoText';
 
@@ -22,46 +22,46 @@ function Navbar() {
   );
 }
 
-const createLink = ({ text, to, ...rest }) => {
-  const className = 'nav-link';
-  if (to) {
-    return (
-      <Link className={className} to={to} {...rest}>
-        {text}
-      </Link>
-    );
-  }
-  return (
-    <span
-      role='button'
-      className={className}
-      style={{ cursor: 'pointer' }}
-      {...rest}
-    >
-      {text}
-    </span>
-  );
-};
+// const createLink = ({ text, to, ...rest }) => {
+//   const className = 'nav-link';
+//   if (to) {
+//     return (
+//       <Link className={className} to={to} {...rest}>
+//         {text}
+//       </Link>
+//     );
+//   }
+//   return (
+//     <span
+//       role='button'
+//       className={className}
+//       style={{ cursor: 'pointer' }}
+//       {...rest}
+//     >
+//       {text}
+//     </span>
+//   );
+// };
 
-function NavLinks() {
-  const { isLoggedIn, logout } = useAuth();
-  const links = [];
-  if (isLoggedIn) {
-    links.push({ text: 'Profile', to: '/profile' });
-    links.push({ text: 'Logout', onClick: () => logout() });
-  } else {
-    links.push({ text: 'Signup', to: '/signup' });
-    links.push({ text: 'Login', to: '/login' });
-  }
-  return (
-    <ul className='navbar-nav'>
-      {links.map((link, i) => (
-        <li key={i} className='nav-item'>
-          {createLink(link)}
-        </li>
-      ))}
-    </ul>
-  );
-}
+// function NavLinks() {
+//   const { isLoggedIn, logout } = useAuth();
+//   const links = [];
+//   if (isLoggedIn) {
+//     links.push({ text: 'Profile', to: '/profile' });
+//     links.push({ text: 'Logout', onClick: () => logout() });
+//   } else {
+//     links.push({ text: 'Signup', to: '/signup' });
+//     links.push({ text: 'Login', to: '/login' });
+//   }
+//   return (
+//     <ul className='navbar-nav'>
+//       {links.map((link, i) => (
+//         <li key={i} className='nav-item'>
+//           {createLink(link)}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
 
 export default Navbar;
