4a391b18b886d7e881482d42f098427cbb2d1cf1
See Shell <323seeshell@gmail.com>
2020-05-17 10:27:37 -0700

    schema and seed updates

--- models/Tutor.js
+++ models/Tutor.js
@@ -2,100 +2,90 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
 
-const TutorSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-      index: {
-        unique: true
-      }
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    },
-    firstName: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    lastName: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    bio: {
-      type: String
-    },
-    subjects: {
-      type: [String]
-    },
-    minGroupSize: {
-      type: Number
-    },
-    maxGroupSize: {
-      type: Number
-    },
-    timeFrame: {
-      type: [String]
-    },
-    age: {
-      type: [String]
-    },
-    education: {
-      type: [String]
-    },
-    rating: {
-      type: Number
-    },
-    role: {
-      type: String,
-      defaultValue: 'tutor'
-    },
-    price: {
-      type: Number
-    },
-    sessionsCompleted: {
-      type: Number
-    },
-    chats: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Chat'
-      }
-    ],
-    reviews: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Review'
-      }
-    ],
-    tutorSessions: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Session'
-      }
-    ]
-  }
-  // { timestamps: true }
-);
-
-// Execute before each user.save() call
-TutorSchema.pre('save', function (callback) {
-  const user = this;
-  user.createdAt = new Date(Date.now());
-  console.log('test hello');
-  callback();
+const TutorSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    index: {
+      unique: true
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String
+  },
+  bio: {
+    type: String
+  },
+  subjects: {
+    type: [String]
+  },
+  minGroupSize: {
+    type: Number
+  },
+  maxGroupSize: {
+    type: Number
+  },
+  timeFrame: {
+    type: [String]
+  },
+  age: {
+    type: [String]
+  },
+  education: {
+    type: [String]
+  },
+  rating: {
+    type: Number
+  },
+  role: {
+    type: String,
+    defaultValue: 'tutor'
+  },
+  price: {
+    type: Number
+  },
+  chats: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Chat'
+    }
+  ],
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review'
+    }
+  ],
+  tutorSessions: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Session'
+    }
+  ]
 });
 
+// Execute before each user.save() call
 TutorSchema.pre('save', function (callback) {
   const user = this;
   // user.createdAt = new Date(Date.now());
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,31 +1,31 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-// const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/agoraDB';
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/agoraDB';
 
-// const dbOptions = {
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true
-// };
+const dbOptions = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+};
 
-// mongoose
-//   .connect(mongoUri, dbOptions)
-//   .then(() => console.log('MongoDB Connected!'))
-//   .catch((err) => console.error(err));
+mongoose
+  .connect(mongoUri, dbOptions)
+  .then(() => console.log('MongoDB Connected!'))
+  .catch((err) => console.error(err));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/agoraDB');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/agoraDB');
 
 const tutorSeed = [
   {
     email: 'courtney@mail.com',
     password: 'test',
-    createdAt: '',
     firstName: 'Courtney',
     lastName: 'Baldwin',
     bio:
       'Hi! I recently completed my Masters degree in 18th century British literature and I am taking a year off before starting my PhD. I have been a nanny, TA, and tutor for 5 years and love to teach. I am willing to take one-on-one or on-going sessions through August 2020.',
+    image: 'https://randomuser.me/api/portraits/women/29.jpg',
     subjects: ['english', 'world history', 'grammar'],
     minGroupSize: 1,
     maxGroupSize: 10,
@@ -35,7 +35,6 @@ const tutorSeed = [
     rating: 4.8,
     role: 'tutor',
     price: 50,
-    sessionsCompleted: 20,
     chats: [],
     reviews: [],
     tutorSessions: []
@@ -43,11 +42,11 @@ const tutorSeed = [
   {
     email: 'tracy@mail.com',
     password: 'test',
-    // createdAt: '',
     firstName: 'Tracy',
     lastName: 'Clark',
     bio:
       'I graduated from UCSD last year with a degree in Early Child Development. I am currently doing teacher training as a middle school math teacher and I enjoy finding ways to make math accessible and fun!',
+    image: 'https://randomuser.me/api/portraits/women/8.jpg',
     subjects: ['algebra', 'geometry', 'trigonometry'],
     minGroupSize: 1,
     maxGroupSize: 5,
@@ -57,7 +56,6 @@ const tutorSeed = [
     rating: 4,
     role: 'tutor',
     price: 30,
-    sessionsCompleted: [],
     chats: [],
     reviews: [],
     tutorSessions: []
@@ -65,11 +63,11 @@ const tutorSeed = [
   {
     email: 'matthew@mail.com',
     password: 'test',
-    // createdAt: 1589689166924,
     firstName: 'Matthew',
     lastName: 'Bishop',
     bio:
       'Self-taught web developer. I have many youtube tutorials on web-development and other coding projects. I love engaging with people to help on their projects or help debug. Check me out at thispersondoesnotexist.com - happy hacking!',
+    image: 'https://randomuser.me/api/portraits/men/44.jpg',
     subjects: ['computer science', 'web development'],
     minGroupSize: 1,
     maxGroupSize: 10,
@@ -79,7 +77,6 @@ const tutorSeed = [
     rating: 4.5,
     role: 'tutor',
     price: 60,
-    sessionsCompleted: [],
     chats: [],
     reviews: [],
     tutorSessions: []
@@ -87,11 +84,11 @@ const tutorSeed = [
   {
     email: 'emmett@mail.com',
     password: 'test',
-    // createdAt: 1589689994853,
     firstName: 'Emmett',
     lastName: 'Gross',
     bio:
       'Home School Experience! I home-schooled my kids plus a small group from the neighborhood through highschool. They are all in college now and I am looking to take on new on-going home-school courses. I have a PhD in Physics from University of Washington and specialize in teaching science and math. I am open to other subjects or advanced courses for curious kids.',
+    image: 'https://randomuser.me/api/portraits/men/21.jpg',
     subjects: [
       'chemistry',
       'physics',
@@ -102,14 +99,14 @@ const tutorSeed = [
       'trigonometry',
       'calculus'
     ],
-    groupSize: 10,
+    minGroupSize: 1,
+    maxGroupSize: 10,
     timeFrame: ['on-going'],
     age: 18,
     education: ['undergraduate', 'master', 'doctorate'],
     rating: 5,
     role: 'tutor',
     price: 80,
-    sessionsCompleted: [],
     chats: [],
     reviews: [],
     tutorSessions: []
@@ -117,20 +114,20 @@ const tutorSeed = [
   {
     email: 'adam@mail.com',
     password: 'test',
-    // createdAt: 1589691162150,
     firstName: 'Adam',
     lastName: 'Morris',
     bio:
       '"History is the record of places and events, art history is the record of how people felt about it." I\'m passionate about art and arts education and would be interested in setting up on-going sessions for interested groups while I finish my PhD in Renaissance Art. Flexible schedule.',
+    image: 'https://randomuser.me/api/portraits/men/33.jpg',
     subjects: ['art', 'art history'],
-    groupSize: 5,
+    minGroupSize: 1,
+    maxGroupSize: 5,
     timeFrame: ['on-going'],
     age: 100,
     education: ['undergraduate', 'masters', 'doctorate'],
     rating: 4.6,
     role: 'tutor',
     price: 90,
-    sessionsCompleted: [],
     chats: [],
     reviews: [],
     tutorSessions: []
@@ -138,20 +135,20 @@ const tutorSeed = [
   {
     email: 'carmen@mail.com',
     password: 'test',
-    // createdAt: 1589691465437,
     firstName: 'Carmen',
     lastName: 'Wells',
     bio:
       "Hola, I'm Carmen from Havana, Cuba. I have taught Spanish in middle and high school for 20 years. I am recently retired and flexible for a day or evening sessions.",
+    image: 'https://randomuser.me/api/portraits/women/4.jpg',
     subjects: ['spanish'],
-    groupSize: 5,
+    minGroupSize: 1,
+    maxGroupSize: 5,
     timeFrame: ['one-time', 'on-going'],
     age: 100,
     education: ['undergraduate'],
     rating: 4.9,
     role: 'tutor',
     price: 40,
-    sessionsCompleted: [],
     chats: [],
     reviews: [],
     tutorSessions: []
@@ -162,65 +159,61 @@ const studentSeed = [
   {
     email: 'ollie@mail.com',
     password: 'test',
-    // createAt: 1589725116167,
     firstName: 'Ollie',
     lastName: 'Rice',
     age: 13,
     education: 'middle',
     role: 'student',
     favorites: [],
-    sessionsCompleted: [],
+    sessionsCompleted: 2,
     chats: [],
     tutorSessions: []
   },
   {
     email: 'norma@mail.com',
     password: 'test',
-    // createAt: 1589725354450,
     firstName: 'Norma',
     lastName: 'Moody',
     age: 17,
     education: 'high',
     role: 'student',
     favorites: [],
-    sessionsCompleted: [],
+    sessionsCompleted: 3,
     chats: [],
     tutorSessions: []
   },
   {
     email: 'david@mail.com',
     password: 'test',
-    // createAt: 1589725455764,
     firstName: 'David',
     lastName: 'Morrison',
     age: 15,
     education: 'high',
     role: 'student',
     favorites: [],
-    sessionsCompleted: [],
+    sessionsCompleted: 6,
     chats: [],
     tutorSessions: []
   },
   {
     email: 'jeannie@mail.com',
     password: 'test',
-    // createAt: 1589725559566,
     firstName: 'Jeannie',
     lastName: 'Watts',
     age: 20,
     education: 'undergraduate',
     role: 'student',
     favorites: [],
-    sessionsCompleted: [],
+    sessionsCompleted: 1,
     chats: [],
     tutorSessions: []
   }
 ];
 
 db.Tutor.deleteMany({})
-  .then(() => db.Tutor.collection.insertMany(tutorSeed))
+  .then(() => db.Tutor.insertMany(tutorSeed))
   .then((data) => {
-    console.log(data.result.n + ' records inserted!');
+    console.log(data.length + ' records inserted!');
     process.exit(0);
   })
   .catch((err) => {
@@ -229,9 +222,9 @@ db.Tutor.deleteMany({})
   });
 
 db.Student.deleteMany({})
-  .then(() => db.Student.collection.insertMany(studentSeed))
+  .then(() => db.Student.insertMany(studentSeed))
   .then((data) => {
-    console.log(data.result.n + ' records inserted!');
+    console.log(data.length + ' records inserted!');
     process.exit(0);
   })
   .catch((err) => {
