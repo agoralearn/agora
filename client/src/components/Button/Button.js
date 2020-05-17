2592ee01d4a66313bb674ad2cfead51bee05aa18
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-05-16 22:19:07 -0700

    add onclick prop to buttons

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Button.scss';
 
-export default function Button({ children, className }) {
-  return <button className={`btn ${className}`}>{children}</button>;
+export default function Button({ children, className, onClick }) {
+  return (
+    <button onClick={onClick} className={`btn ${className}`}>
+      {children}
+    </button>
+  );
 }
 
-Button.Link = function ({ children, className }) {
-  return <button className={`btn btn-link ${className}`}>{children}</button>;
+Button.Link = function ({ children, className, onClick }) {
+  return (
+    <button onClick={onClick} className={`btn btn-link ${className}`}>
+      {children}
+    </button>
+  );
 };
