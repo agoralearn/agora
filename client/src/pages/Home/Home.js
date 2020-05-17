912f44ebc362cd236339fe8e1ab673af7888a141
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-05-16 17:15:13 -0700

    add elements from mockup

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.scss';
 import Hero from '../../components/Hero/Hero';
 import PageHeader from '../../components/PageHeader/PageHeader';
+import Section from '../../components/Section/Section';
 
 export default function Home() {
   return (
@@ -10,14 +11,22 @@ export default function Home() {
       <Hero>
         <Hero.Content>
           <PageHeader>
-            <h1 className='color-primary f-w-l'>A New Way To Learn</h1>
+            <h1 className='color-primary'>A New Way To Learn</h1>
           </PageHeader>
           <p className='color-primary f-w-l'>
             Receive one on one tutoring or find a tutor for your group from our
             many talented tutors
           </p>
+          <h3 className='color-primary u-m'>Sign Up As A...</h3>
         </Hero.Content>
       </Hero>
+
+      {/* How It works */}
+      <Section>
+        <PageHeader>
+          <h2>How It Works</h2>
+        </PageHeader>
+      </Section>
     </div>
   );
 }
