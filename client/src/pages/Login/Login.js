3bb6a3d199f0bf1de958ce662af62e7aacc91fee
Ryan Harris <harris.ucla@gmail.com>
2020-05-18 18:08:24 -0700

    Login page and styling

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -3,7 +3,8 @@ import { Link, Redirect, useHistory } from 'react-router-dom';
 import { useAuth } from '../../utils/auth';
 import PageHeader from '../../components/PageHeader/PageHeader';
 import Button from '../../components/Button/Button';
-import { Container, Form } from 'semantic-ui-react';
+import { Container, Form, Header } from 'semantic-ui-react';
+import './Login.scss';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -32,35 +33,40 @@ function Login() {
         <h1>Login</h1>
       </PageHeader>
       <Form onSubmit={handleFormSubmit}>
-        <Form.Field>
-          <label htmlFor='email'>Email address:</label>
-          <input
-            placeholder='Email...'
-            name='email'
-            type='email'
-            id='email'
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <label htmlFor='pwd'>Password:</label>
-          <input
-            placeholder='Password...'
-            name='password'
-            type='password'
-            id='pwd'
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </Form.Field>
-        <Button type='submit' className='btn btn-primary'>
-          Submit
-        </Button>
+        <div className='Login-form'>
+          <Form.Field>
+            <label htmlFor='email'>Email address:</label>
+            <input
+              placeholder='Email...'
+              name='email'
+              type='email'
+              id='email'
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor='pwd'>Password:</label>
+            <input
+              placeholder='Password...'
+              name='password'
+              type='password'
+              id='pwd'
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </Form.Field>
+          <Button type='submit' className='btn btn-primary'>
+            Submit
+          </Button>
+          <Header as='h4'>
+            Don't have an account?{' '}
+            <Link to='signup'>
+              <Button.Link> Signup here</Button.Link>
+            </Link>
+          </Header>
+        </div>
       </Form>
-      <p>
-        <Link to='/signup'>Don't have an account? Signup here</Link>
-      </p>
     </Container>
   );
 }
