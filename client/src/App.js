a8e508975467ebeea84ddf54dbd6fd0806bdd497
jkaplanis <johntkaplanis@gmail.com>
2020-05-18 20:22:37 -0700

    import tutorbio in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,9 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup/Signup';
 import Chat from './pages/Chat/Chat';
 // import ProtectedRouteComp from './pages/ProtectedRoute';
-// import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import NavBar from './components/Navbar/Navbar';
+import TutorBio from './pages/TutorBio/TutorBio';
 
 function ProtectedRoute({ children, ...rest }) {
   const { isLoggedIn } = useAuth();
