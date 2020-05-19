dcfd4af09c349169277493c661e9e60408d0f257
Ryan Harris <harris.ucla@gmail.com>
2020-05-19 15:56:18 -0700

    Initial search page filter layout

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -2,10 +2,76 @@ import React, { useState, useEffect } from 'react';
 import PageHeader from '../../components/PageHeader/PageHeader';
 import TutorCard from '../../components/TutorCard/TutorCard';
 import API from '../../utils/API';
-import { Container } from 'semantic-ui-react';
+import { Container, Form, Dropdown } from 'semantic-ui-react';
+import './Search.scss';
 
 function Search() {
   const [tutors, setTutors] = useState([]);
+
+  const subjectArray = [
+    'All',
+    'english',
+    'world history',
+    'grammar',
+    'algebra',
+    'geometry',
+    'trigonometry',
+    'computer science',
+    'web development',
+    'chemistry',
+    'physics',
+    'biology',
+    'Earth Science',
+    'calculus',
+    'art',
+    'art history',
+    'spanish'
+  ];
+
+  const subjects = subjectArray.map((subject) => ({
+    key: subject,
+    value: subject,
+    text: subject
+  }));
+
+  const educationArray = [
+    'All',
+    'high school',
+    'undergraduate',
+    'masters',
+    'doctorate'
+  ];
+
+  const educations = educationArray.map((education) => ({
+    key: education,
+    value: education,
+    text: education
+  }));
+
+  const prices = [
+    { key: 'All', value: 'All', text: 'Any' },
+    { key: '10', value: '10', text: '$10/hr' },
+    { key: '20', value: '20', text: '$20/hr' },
+    { key: '50', value: '50', text: '$50/hr' },
+    { key: '75', value: '75', text: '$75/hr' },
+    { key: '100', value: '100', text: '$100/hr' }
+  ];
+
+  const groupSizeArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10+'];
+
+  const groupSizes = groupSizeArray.map((size) => ({
+    key: size,
+    value: size,
+    text: size
+  }));
+
+  const ratings = [
+    { key: '1', value: '1', text: '1+' },
+    { key: '2', value: '2', text: '2+' },
+    { key: '3', value: '3', text: '3+' },
+    { key: '4', value: '4', text: '4+' },
+    { key: '5', value: '5', text: '5+' }
+  ];
 
   useEffect(() => {
     API.getTutors()
@@ -16,6 +82,7 @@ function Search() {
   const renderTutors = () => {
     return tutors.map((tutor) => (
       <TutorCard
+        key={tutor._id}
         {...tutor}
         name={`${tutor.firstName} ${tutor.lastName}`}
         profileImg={tutor.image}
@@ -26,9 +93,65 @@ function Search() {
   return (
     <Container>
       <PageHeader>
-        <h2>Search for Tutors</h2>
+        <h2>Browse Tutors</h2>
       </PageHeader>
-      {renderTutors()}
+      <Form>
+        <Form.Field>
+          <label htmlFor='subject'>Subject</label>
+          <Dropdown
+            id='subject'
+            placeholder='Select Subject'
+            fluid
+            selection
+            options={subjects}
+          />
+        </Form.Field>
+        <Form.Group widths='equal'>
+          <Form.Field>
+            <label htmlFor='education'>Education Level</label>
+            <Dropdown
+              id='education'
+              placeholder='Select Education'
+              fluid
+              selection
+              options={educations}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor='price'>Max Price</label>
+            <Dropdown
+              id='price'
+              placeholder='Select Price'
+              fluid
+              selection
+              options={prices}
+            />
+          </Form.Field>
+        </Form.Group>
+        <Form.Group widths='4'>
+          <Form.Field>
+            <label htmlFor='group'>Group Size</label>
+            <Dropdown
+              id='group'
+              fluid
+              selection
+              defaultValue='1'
+              options={groupSizes}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor='rating'>Rating</label>
+            <Dropdown
+              id='rating'
+              fluid
+              selection
+              defaultValue='3'
+              options={ratings}
+            />
+          </Form.Field>
+        </Form.Group>
+      </Form>
+      <div className='Search-results'>{renderTutors()}</div>
     </Container>
   );
 }
