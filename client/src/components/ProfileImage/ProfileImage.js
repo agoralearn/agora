13b893597ba10c376c64693161dd31c518db4dc5
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-05-21 11:21:46 -0700

    set height of image and image container

--- client/src/components/ProfileImage/ProfileImage.js
+++ client/src/components/ProfileImage/ProfileImage.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './ProfileImage.scss';
 
-export default function ProfileImage({ profileImg, style }) {
+export default function ProfileImage({
+  profileImg,
+  style,
+  height = '100px',
+  width = '100px'
+}) {
   return (
-    <div className='image-cropper' style={style}>
-      <img src={profileImg} alt='avater' className='profile-pic'></img>
+    <div
+      className='image-cropper'
+      style={{ ...style, height: height, width: width }}
+    >
+      <img
+        src={profileImg}
+        alt='avater'
+        className='profile-pic'
+        style={{ height: height, width: width }}
+      ></img>
     </div>
   );
 }
