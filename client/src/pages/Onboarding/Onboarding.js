cb87b158f76bab302ac6c74e80a2b1211c1113ba
jkaplanis <johntkaplanis@gmail.com>
2020-05-23 15:54:01 -0700

    render subjects, listen for clicks to update state

--- client/src/pages/Onboarding/Onboarding.js
+++ client/src/pages/Onboarding/Onboarding.js
@@ -14,7 +14,8 @@ import {
   Icon,
   Button
 } from 'semantic-ui-react';
-import { subjects, education } from '../../utils/categoryData';
+import { education } from '../../utils/categoryData';
+import { categories } from '../../utils/subjectData';
 // import Button from '../../components/Button/Button';
 import PageHeader from '../../components/PageHeader/PageHeader';
 
@@ -24,11 +25,61 @@ function Onboarding() {
     progressPercent: 25
   });
 
+  const [subjects, setSubjects] = useState(categories);
+
+  function toggleSelected(e, cat, subcat) {
+    console.log(cat, subcat);
+    const categoryIndex = subjects.findIndex((category) => {
+      console.log(category.header + '-' + cat);
+      return category.header === cat;
+    });
+    console.log(categoryIndex);
+    console.log(subjects);
+    const subCatIndex = subjects[categoryIndex].subcat.findIndex(
+      (subCategory) => {
+        return subCategory.text === subcat;
+      }
+    );
+    console.log(categories[categoryIndex].subcat[subCatIndex].selected);
+  }
+
   function selectCurrentStep() {
     switch (step.currentStep) {
-      case '':
+      case 'subjects':
+        return renderSubjects();
+      case 'tutoring-info':
+        return;
+      case 'bio':
+        return;
+      case 'image':
         return;
     }
+  }
+
+  function renderSubjects() {
+    return categories.map((cat) => {
+      return (
+        <div key={cat.type}>
+          <h2>{cat.header}</h2>
+          {cat.subcat.map((subcat) => {
+            console.log(cat.header);
+            return (
+              <div
+                key={subcat.key}
+                className='u-m-r disp-inline-b'
+                onClick={(e) => {
+                  toggleSelected(e, cat.header, subcat.text);
+                }}
+              >
+                <Badge selected={subcat.selected} type='outline'>
+                  {subcat.text}
+                </Badge>
+              </div>
+            );
+          })}
+        </div>
+      );
+    });
   }
 
   return (
@@ -37,7 +88,7 @@ function Onboarding() {
         <h2>Profile Setup</h2>
       </PageHeader>
       <Container>
-        {selectCurrentStep}
+        {selectCurrentStep()}
         <Progress percent={step.progressPercent} progress />
         <Button animated>
           <Button.Content visible>Next</Button.Content>
